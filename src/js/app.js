ee963adec2386a6cb8a392769f273407e856bf5d
BoogieWoogie91 <49623384+BoogieWoogie91@users.noreply.github.com>
2021-03-19 04:39:12 +0300

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -48,28 +48,71 @@ function newsDeleter() {
   }
 }
 
+function sideBarCloser(starterBtn, sideBar) {
+  const closeBtn = document.querySelector('.closeSideBar');
+  closeBtn.addEventListener('click', () => {
+    sideBar.classList.toggle('active');
+    starterBtn.classList.toggle('active');
+  });
+}
+
+function sideBarStarter(fetchResult) {
+  const starterBtn = document.querySelector('.sideBarSwitch');
+  const sideBar = document.querySelector('.sideBar');
+  starterBtn.classList.add('active');
+  starterBtn.innerHTML = fetchResult.length;
+
+  fetchResult.forEach((element, index) => {
+    const template = `
+    <div class='sideCard'>
+      <a class='sideCardAnchor' href="#${index}">
+        <img src="${element.urlToImage}" alt="">
+        <p>${element.title}</p>
+      </a>
+    </div>
+    `;
+
+    sideBar.insertAdjacentHTML('beforeend', template);
+  });
+
+  starterBtn.addEventListener('click', () => {
+    const sideCard = document.querySelectorAll('.sideCard');
+    sideBar.classList.add('active');
+    starterBtn.classList.remove('active');
+    console.log(sideCard);
+    setTimeout(() => {
+      sideCard.forEach((element, index) => {
+        element.style.transition = `${100 * index}ms`;
+        element.classList.toggle('active');
+      });
+    }, 700);
+  });
+  sideBarCloser(starterBtn, sideBar);
+}
+
 function cardCreator(fetchResult) {
   const parentNode = document.querySelector('#mainArea');
   newsDeleter();
-  fetchResult.forEach((element) => {
+  fetchResult.forEach((element, index) => {
     const date = new Date(Date.parse(element.publishedAt));
     let monthNumber;
     if (date.getMonth() < 10) {
       monthNumber = `0${date.getMonth()}`;
     }
     const template = `
-    <div class='newsCard'>
-      <img src="${element.urlToImage || 'https://picsum.photos/id/1062/5092/3395'}" alt="img">
-      <h2 class='title'>${element.title}</h2>
-      <p class='author'>${element.author || ''}</p>
-      <p class='date'><span>${date.getDate()}</span>-<span>${monthNumber}</span>-<span>${date.getFullYear()}</span>
-      <span>${date.getHours()}:</span><span>${date.getMinutes()}</span></p>
-      <a class='readMoreUrl' href="${element.url}" target="_blank">Read more >>></a>
-      <button class='status'></button>
-    </div>
+      <div id=${index} class='newsCard'>
+        <img src="${element.urlToImage || 'https://picsum.photos/id/1062/5092/3395'}" alt="img">
+        <h2 class='title'>${element.title}</h2>
+        <p class='author'>${element.author || ''}</p>
+        <p class='date'><span>${date.getDate()}</span>-<span>${monthNumber}</span>-<span>${date.getFullYear()}</span>
+        <span>${date.getHours()}:</span><span>${date.getMinutes()}</span></p>
+        <a class='readMoreUrl' href="${element.url}" target="_blank">Read more >>></a>
+        <button class='status'></button>
+      </div>
   `;
     parentNode.insertAdjacentHTML('beforeend', template);
   });
+  sideBarStarter(fetchResult);
   readChecker();
 }
 
